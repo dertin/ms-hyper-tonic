1d0ca86049d06a9f19a9f74a9bade35e8c25ac8c
dertin <dev.dertin@gmail.com>
2022-12-28 15:34:21 -0300

    fix blocking client tonic

--- ms-executor/src/main.rs
+++ ms-executor/src/main.rs
@@ -1,23 +1,49 @@
+use tokio::runtime::{Builder, Runtime};
 use uuid::Uuid;
-use futures::executor::block_on;
 
 // HTTP/1 server - Hyper.rs
-use hyper::server::conn::http1;
-use hyper::body::Bytes;
-use hyper::{body::Incoming as IncomingBody, Request, Response};
 use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::{body::Incoming as IncomingBody, Request, Response};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 // gRPC protos - Tonic
 use protos::httpgrpc::http_client::HttpClient;
-use protos::httpgrpc::{Header, HttpRequest};
-use tonic::transport::Channel;
+use protos::httpgrpc::{Header, HttpRequest, HttpResponse};
 
+type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
+type Result<T, E = StdError> = ::std::result::Result<T, E>;
+
+struct BlockingClient {
+    client: HttpClient<tonic::transport::Channel>,
+    rt: Runtime,
+}
+
+impl BlockingClient {
+    pub fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+    where
+        D: TryInto<tonic::transport::Endpoint>,
+        D::Error: Into<StdError>,
+    {
+        let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+        let client = rt.block_on(HttpClient::connect(dst))?;
+
+        Ok(Self { client, rt })
+    }
+
+    pub fn handle(
+        &mut self,
+        request: impl tonic::IntoRequest<HttpRequest>,
+    ) -> Result<tonic::Response<HttpResponse>, tonic::Status> {
+        self.rt.block_on(self.client.handle(request))
+    }
+}
 
 #[derive(Debug, Clone)]
 struct Svc {
-    grpc_client: HttpClient<Channel>,
+    grpc_client: i32,
 }
 
 async fn util_body_to_vec(req: Request<IncomingBody>) -> Vec<u8> {
@@ -36,6 +62,7 @@ impl hyper::service::Service<Request<IncomingBody>> for Svc {
     >;
 
     fn call(&mut self, incoming_request: Request<IncomingBody>) -> Self::Future {
+        //let rt  = Runtime::new().unwrap();
 
         println!(
             "received request! method: {:?}, url: {:?}, headers: {:?}",
@@ -54,8 +81,7 @@ impl hyper::service::Service<Request<IncomingBody>> for Svc {
         };
 
         // Await the whole body to be collected into a single `Bytes`...
-        let future_body = util_body_to_vec(incoming_request);
-        let str_body = block_on(future_body);
+        //let str_body = rt.block_on(util_body_to_vec(incoming_request));
 
         // make gRPC request - http incoming request to grpc outgoing request
         let grpc_request = tonic::Request::new(HttpRequest {
@@ -63,15 +89,18 @@ impl hyper::service::Service<Request<IncomingBody>> for Svc {
             version: str_version,
             method: str_method,
             uri: str_uri,
-            body: str_body,
+            body: vec![1],
             headers: vec![vec_headers],
         });
 
-        println!("Sending request to gRPC Server...");
-        
-        let future_grpc_response = self.grpc_client.handle(grpc_request);
-        let grpc_response = block_on(future_grpc_response).unwrap();
-        println!("RESPONSE={:?}", grpc_response);
+        std::thread::spawn(|| {
+            println!("Sending request to gRPC Server...");
+            let mut grpc_client = BlockingClient::connect("http://[::1]:50051").unwrap();
+            let grpc_response = grpc_client.handle(grpc_request).unwrap();
+            println!("RESPONSE={:?}", grpc_response);
+        })
+        .join()
+        .expect("Thread panicked");
 
         Box::pin(async { Ok(Response::new(Full::new(Bytes::from("Hello, World!")))) })
     }
@@ -79,34 +108,23 @@ impl hyper::service::Service<Request<IncomingBody>> for Svc {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    
-    // Listener HTTP Server - Hyper.rs 
+    // Listener HTTP Server - Hyper.rs
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
     let listener = TcpListener::bind(addr).await?;
-    
-    // New gRPC client - Tonic
-    let endpoints = ["http://[::1]:50051", "http://[::1]:50052"]
-        .iter()
-        .map(|a| Channel::from_static(a));
-    let channel = Channel::balance_list(endpoints);
-    let grpc_client = HttpClient::new(channel);
 
     // We start a loop to continuously accept incoming connections
-    
-        loop {
-            let (stream, _) = listener.accept().await?;
-            
-            let grpc_client_clone = grpc_client.clone();
-            println!("{:?}", grpc_client_clone);
-            
-            // Spawn a tokio task to serve multiple connections concurrently
-            tokio::task::spawn(async move {
-                if let Err(err) = http1::Builder::new()
-                    .serve_connection(stream, Svc { grpc_client: grpc_client_clone })
-                    .await
-                {
-                    println!("Error serving connection: {:?}", err);
-                }
-            });
-        }
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, Svc { grpc_client: 0 })
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
